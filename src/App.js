f4f76783759aa3b33fb558fe1acaab097dda8f5c
mervyn <mail.wyrm@gmail.com>
2019-10-14 20:54:40 +0800

    Add ReactMapGL and Navbar.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,38 @@
-import React from "react";
-import logo from "./logo.svg";
-import "App.css";
+/** @jsx jsx */
+import React, { useState } from "react";
+import { css, jsx } from "@emotion/core";
+import { Navbar, Alignment } from "@blueprintjs/core";
+import ReactMapGL from "react-map-gl";
+import "mapbox-gl/dist/mapbox-gl.css";
+import "normalize.css";
+import "@blueprintjs/core/lib/css/blueprint.css";
+import "@blueprintjs/icons/lib/css/blueprint-icons.css";
+
+let divStyle = css`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+`;
 
 function App() {
+  const [viewport, setViewport] = useState({
+    width: "100%",
+    height: "100%",
+    zoom: 1.5
+  });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
+    <div css={divStyle}>
+      <Navbar className="bp3-dark">
+        <Navbar.Group align={Alignment.LEFT}>
+          <Navbar.Heading>Panorama</Navbar.Heading>
+        </Navbar.Group>
+      </Navbar>
+      <ReactMapGL
+        {...viewport}
+        mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_ACCESS_TOKEN}
+        onViewportChange={viewport => setViewport(viewport)}
+      />
     </div>
   );
 }
