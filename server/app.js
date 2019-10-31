fa56c72cb75ee6c536bb550c5def03edef693f6a
mervyn <mail.wyrm@gmail.com>
2019-10-31 20:12:46 +0800

    Enable authentication with Twitter

--- server/app.js
+++ server/app.js
@@ -1,6 +1,26 @@
 require("dotenv").config();
 const express = require("express");
+const Twit = require("twit");
 const app = express();
+
+const T = new Twit({
+  consumer_key: process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+  access_token: process.env.TWITTER_ACCESS_TOKEN,
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+});
+
+T.get(
+  "account/verify_credentials",
+  {
+    include_entities: false,
+    skip_status: true,
+    include_email: false
+  },
+  () => {
+    console.log("Authentication successful.");
+  }
+);
 
 const port = process.env.PORT || 3001;
 app.listen(port, () => console.log(`Listening on port ${port}`));
