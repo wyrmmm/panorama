16fdef59831f2cfd5ef26ce71607fbc28e9103b0
mervyn <mail.wyrm@gmail.com>
2019-11-16 16:54:42 +0800

    Add client build folder and files

---
+++ client/build/precache-manifest.8a024213b06e5a091b5b7d1bf1fb731a.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27f2bdd3e32e4f932add479f08dbf974",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2f3d7bc8c4844e6823b8",
+    "url": "/static/css/2.1e8aa2f5.chunk.css"
+  },
+  {
+    "revision": "2f3d7bc8c4844e6823b8",
+    "url": "/static/js/2.cd9ca0ab.chunk.js"
+  },
+  {
+    "revision": "4a79cae7897ecfe5d181",
+    "url": "/static/js/main.86ba12b0.chunk.js"
+  },
+  {
+    "revision": "6e83da9a938147b4c560",
+    "url": "/static/js/runtime-main.e303b3e9.js"
+  },
+  {
+    "revision": "b62ce8316edcf6cd91b261e1b6db07ea",
+    "url": "/static/media/icons-16.b62ce831.eot"
+  },
+  {
+    "revision": "e0aef47b375af9a5e8a601ea48e74a45",
+    "url": "/static/media/icons-16.e0aef47b.ttf"
+  },
+  {
+    "revision": "e2458cd59cf9786664a5fb610d6f19f1",
+    "url": "/static/media/icons-16.e2458cd5.woff"
+  },
+  {
+    "revision": "748778e0d622390feff65d4086a7cf13",
+    "url": "/static/media/icons-20.748778e0.ttf"
+  },
+  {
+    "revision": "95f6660eb5ef987149ab2e79dfe47046",
+    "url": "/static/media/icons-20.95f6660e.eot"
+  },
+  {
+    "revision": "9f1e61641e0a1d3f70d4bbaa66f5122c",
+    "url": "/static/media/icons-20.9f1e6164.woff"
+  }
+]);
